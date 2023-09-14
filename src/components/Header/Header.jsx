234f293a6f4d5bbd796ab7f859e43e8bb59ca7f1
hungaria <hungaria@student.21-school.ru>
2023-09-14 19:40:12 +0300

    111

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from "../Button/Button";
 import {useTelegram} from "../../hooks/useTelegram";
-import './Header.css
+import './Header.css';
 
 const Header = () => {
     const {user, onClose} = useTelegram();
